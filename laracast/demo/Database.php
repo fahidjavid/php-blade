489928dfe6fec13f0aa12bc5c9a66e54e32a1e23
Fahid Javid <fahidjavid@gmail.com>
2024-08-20 12:28:35 +0500

    Extracted a PHP database class.

---
+++ laracast/demo/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+// connect to our MySQL database and execute a query.
+class Database {
+
+	public $connection;
+
+	public function __construct($config) {
+
+		$dsn = 'mysql:' . http_build_query($config, '', ';');
+
+		$this->connection = new PDO( $dsn, 'root', '', [
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+		] );
+	}
+
+	public function query( $query ) {
+
+
+		$statement = $this->connection->prepare( $query );
+
+		$statement->execute();
+
+		return $statement;
+	}
+}
--- laracast/demo/index.php
+++ laracast/demo/index.php
@@ -1,22 +1,20 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 //require 'router.php';
 
-// connect to our MySQL database.
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 
-$pdo = new PDO($dsn);
+$config = [
+	'host'    => 'localhost',
+	'port'    => 3306,
+	'dbname'  => 'myapp',
+	'charset' => 'utf8mb4'
+];
 
-$statement = $pdo->prepare("select * from posts");
+$db    = new Database($config);
+$posts = $db->query( "select * from posts" )->fetchAll( PDO::FETCH_ASSOC );
 
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
+foreach ( $posts as $post ) {
 	echo '<li>' . $post['title'] . '</li>';
 }
-
-//dd($posts);
-
