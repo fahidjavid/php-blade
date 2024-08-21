e169f597f15090a8cc34fac9f2afcbcdf6b4da61
Fahid Javid <fahidjavid@gmail.com>
2024-08-21 12:40:36 +0500

    Secured SQL injection.

--- laracast/demo/index.php
+++ laracast/demo/index.php
@@ -8,7 +8,10 @@ require 'Database.php';
 $config = require_once "config.php";
 
 $db    = new Database( $config['database'] );
-$posts = $db->query( "select * from posts" )->fetchAll( PDO::FETCH_ASSOC );
+
+$query = "select * from posts where id = :d";
+
+$posts = $db->query( $query, [':d' => 2] )->fetchAll( PDO::FETCH_ASSOC );
 
 foreach ( $posts as $post ) {
 	echo '<li>' . $post['title'] . '</li>';
