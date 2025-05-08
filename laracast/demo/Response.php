f991ea226029571cc9acf52645927faefa492eb6
Fahid Javid <fahidjavid@gmail.com>
2025-05-08 14:36:48 +0500

    Cleared the directory.

--- laracast/demo/Response.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Response {
-	const NOTFOUND = 404;
-	const FORBIDDEN = 403;
-}
--- laracast/demo/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'functions.php';
-require 'Database.php';
-require 'Response.php';
-require 'router.php';
-
-//$config = require_once "config.php";
-//$db    = new Database( $config['database'] );
