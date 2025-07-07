421777081f300b0082a5bd308a409ccad2eb1266
Fahid Javid <fahidjavid@gmail.com>
2025-07-07 12:20:44 +0500

    Added class constants and access them in the index.php.

---
+++ lphptrw/2.6/app/PaymentGateway/Paddle/Transaction.php
@@ -0,0 +1,15 @@
+<?php
+declare( strict_types = 1 );
+
+namespace App\PaymentGateway\Paddle;
+
+class Transaction {
+
+	public const STATUS_PAID = 'paid';
+	public const STATUS_PENDING = 'pending';
+	public const STATUS_DECLINED = 'declined';
+
+	public function __construct() {
+		echo 'Paddle transaction class initialized.';
+	}
+}
