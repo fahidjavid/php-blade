d07def1c3e5ce613f5c0090d4f3a41d8cf6de66f
Fahid Javid <fahidjavid@gmail.com>
2025-07-07 12:39:07 +0500

    Improved Paddel Transaction class to use Enums Status class.

--- lphptrw/2.6/app/PaymentGateway/Paddle/Transaction.php
+++ lphptrw/2.6/app/PaymentGateway/Paddle/Transaction.php
@@ -1,15 +1,25 @@
 <?php
+
 declare( strict_types = 1 );
 
 namespace App\PaymentGateway\Paddle;
 
+use App\Enums\Status;
+
 class Transaction {
 
-	public const STATUS_PAID = 'paid';
-	public const STATUS_PENDING = 'pending';
-	public const STATUS_DECLINED = 'declined';
+	private string $status;
 
 	public function __construct() {
-		echo 'Paddle transaction class initialized.';
+		echo 'Paddle transaction class initialized.' . '<br>';
+	}
+
+
+	public function setStatus( string $status ): void {
+		if ( ! array_key_exists( $status, Status::ALL_STATUSES ) ) {
+			throw new \InvalidArgumentException( "Invalid status: $status" );
+		}
+		$this->status = $status;
+		echo "Status set to: " . Status::ALL_STATUSES[ $status ] . '<br>';
 	}
 }
