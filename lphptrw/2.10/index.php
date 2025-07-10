bcfb8ae25d9a1c871d32ddd8844fc2fc368b1912
Fahid Javid <fahidjavid@gmail.com>
2025-07-10 19:53:04 +0500

    Removed unncessary code.

--- lphptrw/2.10/index.php
+++ lphptrw/2.10/index.php
@@ -5,20 +5,3 @@ declare( strict_types = 1 );
 
 require __DIR__ . '/vendor/autoload.php';
 
-
-$toaster = new \App\Toaster();
-
-$toaster->addSlide( 'Bread' );
-$toaster->addSlide( 'Bread' );
-
-$toaster->toast();
-
-
-$toasterPro = new \App\ToasterPro();
-
-$toasterPro->addSlide( 'Bread' );
-$toasterPro->addSlide( 'Bread' );
-$toasterPro->addSlide( 'Bread' );
-$toasterPro->addSlide( 'Bread' );
-
-$toasterPro->toastBagel();
