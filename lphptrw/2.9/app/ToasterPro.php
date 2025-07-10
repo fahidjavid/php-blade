2e394d5957b518c8d1ab22e593e5b48f457e5dab
Fahid Javid <fahidjavid@gmail.com>
2025-07-10 11:22:04 +0500

    Removed unncessary functions from the ToasterPro class.

--- lphptrw/2.9/app/ToasterPro.php
+++ lphptrw/2.9/app/ToasterPro.php
@@ -2,23 +2,8 @@
 
 namespace App;
 
-class ToasterPro {
-	public array $slices = [];
+class ToasterPro extends Toaster {
 	public int   $size   = 4;
-
-	public function addSlide( string $slice ): void {
-		if ( count( $this->slices ) < $this->size ) {
-			$this->slices[] = $slice;
-		} else {
-			echo "Toaster is full, please remove a slice before adding a new one." . PHP_EOL;
-		}
-	}
-
-	public function toast(): void {
-		foreach ( $this->slices as $i => $slice ) {
-			echo ( $i + 1 ) . ": Toasting: " . $slice . " <br>" . PHP_EOL;
-		}
-	}
 
 	public function toastBagel(): void {
 		foreach ( $this->slices as $i => $slice ) {
