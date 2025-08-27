cd76c7a0fb2ce5a702fa82bb898bd3ea0c1235c9
Fahid Javid <fahidjavid@gmail.com>
2025-08-27 11:58:19 +0500

    Added 2.24 session code.

---
+++ lphptrw/2.24/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$router = new App\Router();
+
+$router->register( '/', [ App\Classes\Home::class, 'index' ] )
+       ->register( '/invoice', [ App\Classes\Invoice::class, 'show' ] )
+       ->register( '/invoice/create', [ App\Classes\Invoice::class, 'create' ] );
+
+try {
+	echo $router->resolve( $_SERVER['REQUEST_URI'] );
+} catch ( \App\Exceptions\RouteNotFoundException $e ) {
+	echo $e->getMessage();
+}
