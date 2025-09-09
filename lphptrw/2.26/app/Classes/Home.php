ab382229b3493171aa52d4da0e516d79e0feac63
Fahid Javid <fahidjavid@gmail.com>
2025-09-09 21:55:33 +0500

    Added index function to display upload form.

--- lphptrw/2.26/app/Classes/Home.php
+++ lphptrw/2.26/app/Classes/Home.php
@@ -3,6 +3,15 @@
 namespace App\Classes;
 
 class Home {
+	public function index(): string {
+		return <<<FORM
+<form action="/upload" method="post" enctype="multipart/form-data">
+	Select image to upload:
+	<input type="file" name="receipt" id="receipt">
+	<input type="submit" value="Upload" name="submit">
+</form>
+FORM;
+	}
 
 	public function upload(): void {
 		var_dump( $_FILES );
