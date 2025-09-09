5483b1fc251699e0bbe7d0afd1b2f3b9e7dec81c
Fahid Javid <fahidjavid@gmail.com>
2025-09-09 21:54:26 +0500

    Removed session echo statement

--- lphptrw/2.26/index.php
+++ lphptrw/2.26/index.php
@@ -12,8 +12,6 @@ $router->get( '/', [ App\Classes\Home::class, 'index' ] )
        ->get( '/invoice/create', [ App\Classes\Invoice::class, 'create' ] )
        ->post( '/invoice/create', [ App\Classes\Invoice::class, 'store' ] );
 
-echo $_SESSION['count'];
-
 try {
 	echo $router->resolve( $_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']) );
 } catch ( \App\Exceptions\RouteNotFoundException $e ) {
